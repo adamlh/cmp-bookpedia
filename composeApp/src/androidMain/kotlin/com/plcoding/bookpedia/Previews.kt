caaa37f6bc42d115a6c909d4e7c634e0a0bdf2ef
Adam Hughes <adamhughes31@gmail.com>
2025-02-18 23:57:29 +0000

    Created search bar. Workingn on Book List Items and Book placeholder image using koil

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,30 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+
+@Preview( showBackground = true)
+@Composable
+private fun BookSearchBarPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        BookSearchBar(
+            searchQuery = "",
+            onSearchQueryChange = {},
+            onImgSearch = {},
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
+
